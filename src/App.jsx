76273872a296ae9ee62bf1f8d59c0861a43e04e7
Italo Bandeira <italobpl94@gmail.com>
2023-08-05 15:30:36 -0300

    Upload project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar from './assets/components/Navbar'
+import Home from './assets/components/Home'
+import Footer from './assets/components/Footer'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
