490f93d56d243522228b8a3d0d8ca0e4f7e13300
Italo Bandeira <italobpl94@gmail.com>
2023-08-06 17:52:02 -0300

    Developing home page

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,13 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function Home() {
+function Home(){
+
+  const [champions, setChampions] = useState([])
+
+  useEffect(() => {
+    const fetchChampions = async () => {
+      const response = await fetch('https://ddragon.leagueoflegends.com/cdn/13.15.1/data/pt_BR/champion.json')
+      const data = await response.json()
+      setChampions(data)
+    }
+    fetchChampions()
+  }, [])
+
   return (
-    <section class="text-gray-400 bg-gray-50 body-font">
-      <div class="container mx-auto py-24">
-        <h1 class="text-center text-2xl font-bold text-black">Escolha seu</h1>
-        <h1 class="text-center text-8xl font-bold text-black">Campeão</h1>
-      </div>
-    </section>
+    <div>
+      <section className="text-gray-400 bg-gray-50 body-font">
+        <div className="container mx-auto py-24">
+          <h1 className="text-center text-2xl font-bold text-black">Escolha seu</h1>
+          <h1 className="text-center text-8xl font-bold text-black">Campeão</h1>
+        </div>
+      </section>
+
+      <section className="text-gray-400 body-font bg-gray-900">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap w-full mb-20">
+            <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+              <form>
+                <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                <div className="relative mb-2">
+                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                      <path stroke="currentColor" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
+                  </div>
+                  <input type="search" id="default-search" className="w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Busque um campeão" />
+                  <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-yellow-600 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-blue-500 font-medium rounded-lg text-sm px-4 py-2 dark:yellow-600 dark:hover:yellow-300 dark:focus:yellow-800">Pesquisar</button>
+                </div>
+              </form>
+              <div className="h-1 bg-yellow-600 rounded"></div>
+            </div>
+          </div>
+          <div className="flex flex-wrap -m-4">
+            <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
+                <img className="h-40 rounded w-full object-cover object-center mb-6" src="https:dummyimage.com/720x400" alt="content" />
+                <h3 className="tracking-widest text-indigo-400 text-xs font-medium title-font">SUBTITLE</h3>
+                <h2 className="text-lg text-white font-medium title-font mb-4"></h2>
+                <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav class="bg-gray-900 shadow-sm">
-      <div class="mx-auto p-1">
-        <div class="relative flex h-16">
-          <div class="flex flex-1 justify-center">
-            <div class="flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" class="league" width="30" height="32" viewBox="0 0 30 32" fill="none">
+    <nav className="bg-gray-900 shadow-sm">
+      <div className="mx-auto p-1">
+        <div className="relative flex h-16">
+          <div className="flex flex-1 justify-center">
+            <div className="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" className="league" width="30" height="32" viewBox="0 0 30 32" fill="none">
                 <g>
                   <path d="M1.80644 9.75049C0.655032 11.8373 0 14.2271 0 16.7683C0 19.3095 0.655032 21.7015 1.80644 23.7883V9.75049Z" fill="#C28F2C"></path>
                   <path d="M15 2.02222C13.7829 2.02222 12.602 2.16921 11.4688 2.43647V4.75718C12.5907 4.44093 13.7738 4.26721 15 4.26721C22.0218 4.26721 27.7153 9.84627 27.7153 16.7305C27.7153 19.8307 26.5571 22.6659 24.6464 24.8463L24.2838 26.118L23.4814 28.9331C27.4184 26.2761 30.0023 21.8195 30.0023 16.7705C30 8.62355 23.2843 2.02222 15 2.02222Z" fill="#C28F2C"></path>
@@ -15,7 +15,7 @@ function Navbar() {
                   <path d="M10.1088 0H1.55029L3.16634 3.29844V28.7038L1.55029 32H21.1922L22.9737 25.7572H10.1088V0Z" fill="#C28F2C"></path>
                 </g>
               </svg>
-              <span class="ml-3 font-bold text-xl text-white">League of Legends</span>
+              <span className="ml-3 font-bold text-xl text-white">League of Legends</span>
             </div>
           </div>
         </div>
